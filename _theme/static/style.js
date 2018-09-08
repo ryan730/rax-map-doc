d04552c81e9dfff292b893c351271f55f95fe325
zhu.yan <zhu.yan@alibaba-inc.com>
2018-09-08 23:45:09 +0800

    Updates

--- _theme/static/style.js
+++
@@ -1,9 +0,0 @@
-import './main.css';
-import './layout.less';
-import './header.less';
-import './cover.less';
-import './highlight.less';
-import './doc.less';
-import './notfound.less';
-import './footer.less';
-import './custom.less';
--- _theme/template/utils.js
+++
@@ -1,9 +0,0 @@
-export const offsetTop = el => {
-  let total = 0;
-  let parent = el;
-  while (parent) {
-    total += parent.offsetTop;
-    parent = parent.offsetParent;
-  }
-  return total;
-};
